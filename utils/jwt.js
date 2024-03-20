c9d118853c6283f8f3f2117f566c38574d9fc61e
ebubekeyz <ebubeofforjoe@gmail.com>
2024-03-20 11:45:05 +0100

    fourty

--- utils/jwt.js
+++ utils/jwt.js
@@ -28,7 +28,7 @@ const attachCookiesToResponse = ({ res, user }) => {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === 'production',
-    domain: 'trex-holding.netlify.app',
+    domain: 'trex-holding-server.com',
     signed: true,
     sameSite: 'None',
   });
