3e8c9a9336cef9fc9b24bb46e533b50ea378e270
ebubekeyz <ebubeofforjoe@gmail.com>
2024-03-21 19:16:51 +0100

    fourty

--- utils/jwt.js
+++ utils/jwt.js
@@ -30,7 +30,8 @@ const attachCookiesToResponse = ({ res, user }) => {
     secure: process.env.NODE_ENV === 'production',
     domain: 'trex-holding-server.com',
     signed: true,
-    sameSite: 'None',
+    sameSite: 'Lax',
+    path: '/',
   });
 };
 
