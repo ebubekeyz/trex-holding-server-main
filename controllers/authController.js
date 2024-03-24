5af3f3dc47af865ae1cfba6916eb8bf16936f2e3
ebubekeyz <ebubeofforjoe@gmail.com>
2024-03-24 07:56:40 +0100

    fourtyone

--- controllers/authController.js
+++ controllers/authController.js
@@ -154,28 +154,28 @@ const login = async (req, res) => {
   res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 
-const logout = async (req, res) => {
-  res.cookie('token', 'logout', {
-    httpOnly: true,
-    expires: new Date(Date.now()),
-  });
-  res.clearCookie();
-  res.status(StatusCodes.OK).json({ msg: 'user logged out' });
-};
-
 // const logout = async (req, res) => {
 //   res.cookie('token', 'logout', {
 //     httpOnly: true,
 //     expires: new Date(Date.now()),
-//     secure: process.env.NODE_ENV === 'production',
-//     domain: 'trex-holding-server.com',
-//     signed: true,
-//     sameSite: 'None',
-//     path: '/',
 //   });
-//   res.clearCookie('token', { path: '/' });
+//   res.clearCookie();
 //   res.status(StatusCodes.OK).json({ msg: 'user logged out' });
 // };
+
+const logout = async (req, res) => {
+  res.cookie('token', 'logout', {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+    secure: process.env.NODE_ENV === 'production',
+    domain: 'trex-holding-server.com',
+    signed: true,
+    sameSite: 'None',
+    path: '/',
+  });
+  res.clearCookie('token', { path: '/' });
+  res.status(StatusCodes.OK).json({ msg: 'user logged out' });
+};
 
 const passwordReset = async (req, res) => {
   const { newPassword, password } = req.body;
